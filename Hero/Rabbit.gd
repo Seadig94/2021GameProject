98023f09d8c4e2de027ee03981c2eb2f07a8433d
Seadig94 <seadig94@kingshigh.school.nz>
2021-06-22 10:52:40 +1200

    22/06/21
    Swag (Enemy explosion)

--- Hero/Rabbit.gd
+++ Hero/Rabbit.gd
@@ -4,6 +4,12 @@ var knockback = Vector2.ZERO
 
 onready var stats = $Stats
 
+func create_enemy_effect():
+	var EnemyEffect = load("res://Resources/Effects/EnemyEffect.tscn")
+	var enemyEffect = EnemyEffect.instance()
+	var world = get_tree().current_scene
+	world.add_child(enemyEffect)
+	enemyEffect.global_position = global_position
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
@@ -12,6 +18,9 @@ func _physics_process(delta):
 func _on_Hurtbox_area_entered(area):
 	stats.health -= area.damage
 	knockback = area.knockback_vector * 120
+	
 
 func _on_Stats_no_health():
+	create_enemy_effect()
 	queue_free()
+
---
+++ Hero/Resources/Effects/Effects.gd
@@ -0,0 +1,5 @@
+extends Node
+
+
+func _on_AnimatedSprite_animation_finished():
+	queue_free() 
